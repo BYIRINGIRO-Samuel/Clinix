592db4b73df07b4521fbeb849e73b420b5dc64b9
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-03-19 09:45:03 +0100

    Patient Portal: integration of PatientDAO.java

---
+++ src/main/java/com/pms/dao/PatientDAO.java
@@ -0,0 +1,19 @@
+package com.pms.dao;
+
+import com.pms.model.Appointment;
+import com.pms.model.Billing;
+import com.pms.model.MedicalRecord;
+import com.pms.model.Prescription;
+import com.pms.model.User;
+import java.util.List;
+
+public interface PatientDAO {
+    List<Appointment> getAppointmentsByPatientId(Long patientId);
+    List<MedicalRecord> getMedicalRecordsByPatientId(Long patientId);
+    List<Prescription> getPrescriptionsByPatientId(Long patientId);
+    List<Billing> getBillingsByPatientId(Long patientId);
+    void bookAppointment(Appointment appointment);
+    void updateProfile(User user);
+    User getPatientById(Long id);
+    List<User> getAllDoctors();
+}
